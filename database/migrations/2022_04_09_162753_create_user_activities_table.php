640681bba0568ca6182ceb582e9bafd98887d6b8
yuke brilliant <yukebrilliant@gmail.com>
2022-04-09 23:32:27 +0700

    Feat: add user activity migration

---
+++ database/migrations/2022_04_09_162753_create_user_activities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('activity_description');
+            $table->string('user_agent');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_activities');
+    }
+};
