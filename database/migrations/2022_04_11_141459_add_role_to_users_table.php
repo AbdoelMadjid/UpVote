42c3738b19be7d448e2d07acb19cb24e67e95f18
yuke brilliant <yukebrilliant@gmail.com>
2022-04-11 21:26:52 +0700

    Feat: add role_id to users table

---
+++ database/migrations/2022_04_11_141459_add_role_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Role;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')
+                ->after('group_id')
+                ->default(Role::IS_USER)
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
